f5086b7c09e639a4f0ccfc0007434e8c1b4570b0
Steven Roper <steven.m.roper@gmail.com>
2019-02-25 15:32:33 -0700

    Add subscriptions to new links and votes.

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -26,6 +26,53 @@ export const FEED_QUERY = gql`
     }
   }
 `;
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+      createdAt
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`;
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      id
+      link {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`;
+
 
 class LinkList extends React.Component {
   
@@ -38,12 +85,42 @@ class LinkList extends React.Component {
     store.writeQuery({ query: FEED_QUERY, data });
   }
 
+  subscribeToNewLinks = async subscribeToMore => {
+    subscribeToMore({
+      document: NEW_LINKS_SUBSCRIPTION,
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data) return prev;
+
+        const newLink = subscriptionData.data.newLink;
+        const exists = prev.feed.links.find(({ id }) => id === newLink.id);
+        if (exists) return prev;
+
+        return Object.assign({}, prev, {
+          feed: {
+            links: [newLink, ...prev.feed.links],
+            count: prev.feed.links.length + 1,
+            __typename: prev.feed.__typename,
+          },
+        });
+      },
+    });
+  }
+
+  subscribeToNewVotes = subscribeToMore => {
+    subscribeToMore({
+      document: NEW_VOTES_SUBSCRIPTION,
+    });
+  }
+
   render() {
     return (
       <Query query={FEED_QUERY}>
-        {({ loading, error, data }) => {
+        {({ loading, error, data, subscribeToMore }) => {
           if (loading) return <div>Loading...</div>;
           if (error) return <div>Oops, there was a problem.</div>;
+
+          this.subscribeToNewLinks(subscribeToMore);
+          this.subscribeToNewVotes(subscribeToMore);
 
           return (
             <div>
